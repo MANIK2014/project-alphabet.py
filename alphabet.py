050ae24df90d2dcafeb344320c5b7127ba4686f3
MANIK2014 <tiwarisunu@gmail.com>
2026-06-10 10:36:55 +0100

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,73 @@
+print("Let's check if your given charecters are alpabet or not!")
+print("First,Enter your character there👇")
+s=(input("Enter your alphabet character here:"))
+#Note:Exept the letter"A",I'll be using the word "Yes for other alphabet charecters and "No" for non-alphabetical charaters.
+#Sec.Note:This code only works if you give the character in upper case.
+if s=="A":
+    print("Ok,That's the first letter of the alphabet!!!")
+elif s=="B":
+    print("Yes")
+elif s=="C":
+    print("Yes")
+elif s=="D":
+    print("Yes")
+elif s=="E":
+    print("Yes")
+elif s=="F":
+    print("Yes")
+elif s=="G":
+    print("Yes")
+elif s=="H":
+    print("Yes")
+elif s=="I":
+    print("Yes")
+elif s=="J":
+    print("Yes")
+elif s=="K":
+    print("Yes")
+elif s=="L":
+    print("Yes")
+elif s=="M":
+    print("Yes")
+elif s=="N":
+    print("Yes")
+elif s=="O":
+    print("Yes")
+elif s=="P":
+    print("Yes")
+elif s=="Q":
+    print("Yes")
+elif s=="R":
+    print("Yes")
+elif s=="S":
+    print("Yes")
+elif s=="T":
+    print("Yes")
+elif s=="U":
+    print("Yes")
+elif s=="V":
+    print("Yes")
+elif s=="W":
+    print("Yes")
+elif s=="X":
+    print("Yes")
+elif s=="Y":
+    print("Yes")
+elif s=="Z":
+    print("Yes")
+else:
+    print("No")                                            
+            
+                            
+                    
+                                                                                    
+            
+                    
+        
+
+        
+        
+        
+
+
+
